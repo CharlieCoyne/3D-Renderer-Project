0c93471d1f1899d1bc6e1df9df6a0a365bc205b4
Charlie <charlie_coyne@outlook.com>
2026-03-18 14:54:53 +0000

    Added Shading to Triangles and Fixed Bugs
    Added lighting and shading to the triangles depending on the orientation of their normal. I also added a new Shape class which stores the set of triangles that make up its shape and also its location in 3d space.
    
    I also fixed the issue with culling triangles behind the camera and additional divide by 0 errors that occurred when walking through a triangle.

--- 3dRenderer.py
+++ 3dRenderer.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 import math
 from Vector3 import *
+from Shape import *
 
 pygame.init()
 
@@ -11,6 +12,8 @@ WIDTH, HEIGHT = 1920, 1080
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("3d Renderer")
 start_time = pygame.time.get_ticks()
+
+clock = pygame.time.Clock()
 
 # Temporary Cube
 cubeTriangles = [
@@ -54,34 +57,55 @@ projectionMatrix[2][2] = fFar / (fFar - fNear)
 projectionMatrix[3][2] = (-fFar * fNear) / (fFar - fNear)
 projectionMatrix[2][3] = 1 # used to get z back out during matrix multiplication
 
+shapes = []
+shapes.append(Shape(Vector3(0,-2,3),cubeTriangles))
+shapes.append(Shape(Vector3(0,-2,4),cubeTriangles))
+shapes.append(Shape(Vector3(1,-2,4),cubeTriangles))
+shapes.append(Shape(Vector3(-1,-2,4),cubeTriangles))
+
 
 def distance_between(point1: Vector3, point2: Vector3) -> Vector3:
     return Vector3(point1.x - point2.x, point1.y - point2.y, point1.z - point2.z)
 
+def brightness_to_grayscale(value: float) -> tuple:
+    ambient = 0.2          # minimum light level
+    diffuse_strength = 0.6 # reduces intensity
+
+    value = max(0, value)
+    value = ambient + diffuse_strength * value
+
+    # gamma correction
+    value = value ** 0.6
+
+    gray = int(min(1, value) * 255)
+    return (gray, gray, gray)
+
 def draw_triangle(x1,y1,x2,y2,x3,y3, colour = (255,255,255)):
-    pygame.draw.line(screen, colour, (x1,y1),(x2,y2), 3)
-    pygame.draw.line(screen, colour, (x2,y2),(x3,y3), 3)
-    pygame.draw.line(screen, colour, (x3,y3),(x1,y1), 3)
+    points = [(x1, y1), (x2, y2), (x3, y3)]
+    pygame.draw.polygon(screen, colour, points)
 
 camera_location = Vector3(0,0,0)
 camera_direction = Vector3(0,0,0)
-speed = 0.01
+light_direction = Vector3(0,0,-1)
+speed = 5
 
 running = True
 while running:
+    delta_time = clock.tick() / 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        camera_location.z += speed
+        camera_location.z += speed * delta_time
     if keys[pygame.K_s]:
-        camera_location.z -= speed
+        camera_location.z -= speed * delta_time
     if keys[pygame.K_a]:
-        camera_location.x -= speed
+        camera_location.x -= speed * delta_time
     if keys[pygame.K_d]:
-        camera_location.x += speed
+        camera_location.x += speed * delta_time
 
     fElapsedTime = (pygame.time.get_ticks() - start_time) / 1000
     fTheta = fElapsedTime
@@ -111,66 +135,99 @@ while running:
     # Start of drawing stuff
 
     # Drawing Triangles
-    for triangle in cubeTriangles:
-        offset = 3 #Temporary offset so that cube isn't rendered inside of the "camera"
+    triangles_to_draw = []
+    for shape in shapes:
+        for triangle in shape.triangles:
+            offset = 3 #Temporary offset so that cube isn't rendered inside of the "camera"
 
-        p0 = Vector3(triangle[0].x, triangle[0].y, triangle[0].z)
-        p1 = Vector3(triangle[1].x, triangle[1].y, triangle[1].z)
-        p2 = Vector3(triangle[2].x, triangle[2].y, triangle[2].z)
+            p0 = Vector3(triangle[0].x, triangle[0].y, triangle[0].z)
+            p1 = Vector3(triangle[1].x, triangle[1].y, triangle[1].z)
+            p2 = Vector3(triangle[2].x, triangle[2].y, triangle[2].z)
 
-        # Applying Z Rotation
-        p0 = p0.do_vector_matrix_multiplication(zRotationMatrix)
-        p1 = p1.do_vector_matrix_multiplication(zRotationMatrix)
-        p2 = p2.do_vector_matrix_multiplication(zRotationMatrix)
+            # Applying Z Rotation
+            #p0 = p0.do_vector_matrix_multiplication(zRotationMatrix)
+            #p1 = p1.do_vector_matrix_multiplication(zRotationMatrix)
+            #p2 = p2.do_vector_matrix_multiplication(zRotationMatrix)
 
-        # Applying X Rotation
-        p0 = p0.do_vector_matrix_multiplication(xRotationMatrix)
-        p1 = p1.do_vector_matrix_multiplication(xRotationMatrix)
-        p2 = p2.do_vector_matrix_multiplication(xRotationMatrix)
-        
-        # Applying offset and projecting to screen
-        p0.x += -camera_location.x
-        p1.x += -camera_location.x
-        p2.x += -camera_location.x
+            # Applying X Rotation
+            #p0 = p0.do_vector_matrix_multiplication(xRotationMatrix)
+            #p1 = p1.do_vector_matrix_multiplication(xRotationMatrix)
+            #p2 = p2.do_vector_matrix_multiplication(xRotationMatrix)
+            
+            # Apply object world position
+            p0.x += shape.position.x
+            p0.y += shape.position.y
+            p0.z += shape.position.z
 
-        p0.z += offset - camera_location.z
-        p1.z += offset - camera_location.z
-        p2.z += offset - camera_location.z
+            p1.x += shape.position.x
+            p1.y += shape.position.y
+            p1.z += shape.position.z
 
-        # Normal calculations
-        line1 = Vector3()
-        line1.x = p1.x - p0.x
-        line1.y = p1.y - p0.y
-        line1.z = p1.z - p0.z
+            p2.x += shape.position.x
+            p2.y += shape.position.y
+            p2.z += shape.position.z
 
-        line2 = Vector3()
-        line2.x = p2.x - p0.x
-        line2.y = p2.y - p0.y
-        line2.z = p2.z - p0.z
+            avg_z = (p0.z + p1.z + p2.z) / 3
+            
+            # Applying object camera position
+            p0.x += -camera_location.x
+            p1.x += -camera_location.x
+            p2.x += -camera_location.x
 
-        normal = line1.get_unit_normal_vector(line2)
-        camera_to_triangle = distance_between(p0, camera_location)
-        dot_product_result =camera_to_triangle.do_dot_product(normal)
+            p0.z += -camera_location.z
+            p1.z += -camera_location.z
+            p2.z += -camera_location.z
 
-        if (dot_product_result < 0):
-            light_direction = Vector3(0,0,-1)
-            unit_light_direction = light_direction.get_unit_vector()
+            # Culls triangles that are too close to avoid divide by zero errors
+            if p0.z <= fNear or p1.z <= fNear or p2.z <= fNear:
+                continue
 
-            light_dot_product = normal.do_dot_product(light_direction)
+            # Normal calculations
+            line1 = Vector3()
+            line1.x = p1.x - p0.x
+            line1.y = p1.y - p0.y
+            line1.z = p1.z - p0.z
 
-            projectedTriangle = [p0.do_vector_matrix_multiplication(projectionMatrix),
-                        p1.do_vector_matrix_multiplication(projectionMatrix),
-                        p2.do_vector_matrix_multiplication(projectionMatrix)]
+            line2 = Vector3()
+            line2.x = p2.x - p0.x
+            line2.y = p2.y - p0.y
+            line2.z = p2.z - p0.z
 
-            # Scaling all values to screen size
-            for p in projectedTriangle:
-                p.x = int((p.x+1)*0.5*WIDTH)
-                p.y = int((1-(p.y+1)*0.5)*HEIGHT)
+            normal = line1.get_unit_normal_vector(line2)
+            dot_product_result = normal.do_dot_product(p0)
 
-            draw_triangle(projectedTriangle[0].x, projectedTriangle[0].y,
-                projectedTriangle[1].x, projectedTriangle[1].y,
-                projectedTriangle[2].x, projectedTriangle[2].y)
+            if (dot_product_result < 0):
+                unit_light_direction = light_direction.get_unit_vector()
 
+                light_dot_product = normal.do_dot_product(light_direction)
+                brightness = (light_dot_product + 1) / 2 # Value 0-1
+                colour = brightness_to_grayscale(brightness)
+
+                projectedTriangle = [p0.do_vector_matrix_multiplication(projectionMatrix),
+                            p1.do_vector_matrix_multiplication(projectionMatrix),
+                            p2.do_vector_matrix_multiplication(projectionMatrix)]
+
+                # Scaling all values to screen size
+                for p in projectedTriangle:
+                    p.x = int((p.x+1)*0.5*WIDTH)
+                    p.y = int((1-(p.y+1)*0.5)*HEIGHT)
+
+                triangles_to_draw.append({
+                "points": projectedTriangle,
+                "colour": colour,
+                "depth": avg_z
+                })
+
+        # Painter's Algorithm i.e draw closest things last
+        triangles_to_draw.sort(key=lambda tri: tri["depth"], reverse=True)
+        for triangle in triangles_to_draw:
+            p = triangle["points"]
+            draw_triangle(
+                p[0].x, p[0].y,
+                p[1].x, p[1].y,
+                p[2].x, p[2].y,
+                triangle["colour"]
+            )
     # End of drawing stuff
 
     # Update display
---
+++ Shape.py
@@ -0,0 +1,6 @@
+from Vector3 import *
+
+class Shape:
+    def __init__(self, position : Vector3, triangles):
+        self.position = position 
+        self.triangles = triangles
